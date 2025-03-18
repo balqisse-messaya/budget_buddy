ae4cb9cab6f2dfe6d3cdf9f1f76ca56676cbb118
morgane-rossi <morgane.rossi@laplateforme.io>
2025-03-18 13:41:22 +0100

    displaying table data with Label widget

---
+++ test.py
@@ -0,0 +1,49 @@
+import mysql.connector
+from os import getenv
+from dotenv import load_dotenv
+from tkinter import *
+
+load_dotenv()
+username = getenv("USERNAME")
+userpass = getenv("PASS")
+
+def display_allproducts():
+
+    try :
+
+        connection = mysql.connector.connect(
+            host = "localhost",
+            user = username,
+            password = userpass,
+            database = "store"
+        )
+        usercurs = connection.cursor()
+
+        usercurs.execute(
+            """
+                SELECT name_prod, name_cat, price AS prix, quantity as quantite
+                FROM product JOIN category 
+                ON product.id_cat = category.id_cat;
+            """
+        )
+        rows = usercurs.fetchall()
+        print(rows)
+        usercurs.close()
+        connection.close()
+        return rows
+
+    except mysql.connector.Error as error:
+        print(f"Something went wrong: {error}")
+
+main_window = Tk()
+
+custom_display = Label(main_window, text="Application inventaire", font=("Helvetica, 18"))
+
+custom_display.pack()
+
+rows = display_allproducts()
+for r in rows :
+    ligne = custom_display = Label(main_window, text=r, font=("Helvetica, 12"))
+    ligne.pack()
+
+main_window.mainloop()
