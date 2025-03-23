85f54f63b83875914d7319a9b2445b8b4ee05f30
morgane-rossi <morgane.rossi@laplateforme.io>
2025-03-23 15:54:56 +0100

    creation database changes

--- create_database.sql
+++ create_database.sql
@@ -1,21 +1,18 @@
-CREATE DATABASE budget_buddy;
-
-USE budget_buddy;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     last_name VARCHAR(50),
     first_name VARCHAR(50),
+    balance FLOAT,
     email VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    balance FLOAT
+    password VARCHAR(255)
 );
 
 CREATE TABLE transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     description VARCHAR(100),
-    category VARCHAR(2100),
+    category VARCHAR(100),
     amount DECIMAL(10, 2),
     date DATE,
     type ENUM('withdrawal', 'deposit', 'transfer'),
